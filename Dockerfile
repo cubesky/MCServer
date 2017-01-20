854fb1b94febd857c20b0caf8d8340131b2694cd
cubesky <max_301@live.com>
2017-01-20 16:49:37 +0800

    add webapi

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ MAINTAINER cubesky
 ADD Server.tar /mcserver
 
 EXPOSE 25565
+EXPOSE 8080
 
 WORKDIR /mcserver/
 VOLUME /mcserver/world
---
+++ build.sh
@@ -0,0 +1,2 @@
+tar -cvf Server.tar config crash-reports libraries mods world eula.txt forge-1.10.2-12.18.3.2185-universal.jar minecraft_server.1.10.2.jar server.properties
+docker build -t nvmc2017w .
